f531f02ea064e3cc504f38b826b63ca011d71cfb
oktamov <mr006developer@gmail.com>
2023-11-29 17:29:23 +0500

    initial commit

--- simple_chat/asgi.py
+++ simple_chat/asgi.py
@@ -12,7 +12,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_chat.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-import chat.routing
 
 application = ProtocolTypeRouter(
     {
--- simple_chat/settings.py
+++ simple_chat/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'daphne',
-    'channels',
+    # 'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
