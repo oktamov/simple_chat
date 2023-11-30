c65472222d9481a8cf11105c668db62997db76f1
oktamov <mr006developer@gmail.com>
2023-11-30 13:17:55 +0500

    initial commit

--- simple_chat/settings.py
+++ simple_chat/settings.py
@@ -137,7 +137,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("161.35.22.7", 6379)],
         },
     },
 }
